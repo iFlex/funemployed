391a174590f67fe50612c62417fc387fe1c39a2e
milorad <milorad.liviu.felix@gmail.com>
2020-04-29 19:47:39 +0100

    feat(tools): added script to transform csv export of excel where we come up with the cards into usable files by the system

---
+++ tools/make_card_deck.py
@@ -0,0 +1,33 @@
+import csv
+import sys
+
+path_to_csv_export = ""
+if len(sys.argv) > 1:
+	path_to_csv_export = sys.argv[1]
+else:
+	path_to_csv_export = input("Path to your CSV export:")
+
+roles_file = "./roles"
+traits_file = "./traits"
+
+with open(path_to_csv_export, newline='') as csvfile:
+	spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
+	
+	jobs =[]
+	traits = []
+
+	skipped_frist = False
+	for row in spamreader:
+		if skipped_frist:
+			jobs.append(row[0])
+			for i in range(1,len(row)):
+				if len(row[i]) > 0:
+					traits.append(row[i])
+		else:
+			skipped_frist = True
+
+with open(roles_file, "w") as w:
+	w.write("\n".join(jobs))
+
+with open(traits_file, "w") as w:
+	w.write("\n".join(traits))
